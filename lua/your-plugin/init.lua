2357188ab2c3d5d6cada46ea0160c9c386aee57d
Daniel Fliegauf <daniel.fliegauf@seitenbau.com>
2024-07-25 23:06:49 +0200

    test

--- lua/your-plugin/init.lua
+++ lua/your-plugin/init.lua
@@ -1,13 +1,5 @@
 local M = {}
 
-local function log(msg)
-	vim.api.nvim_out_write(msg .. " hellol \n")
-end
-
-function M.greet()
-	print("Hello from your Neovim plugin!")
-end
-
 -- Function to notify when plugin is loaded
 function M.notify_on_load()
 	vim.notify("My Plugin is loaded!", vim.log.levels.INFO)
@@ -15,6 +7,5 @@ end
 
 -- Call the function to notify
 M.notify_on_load()
-M.greet()
 
 return M
